98dac1ba3986407e4d7ea1f0af06906395779cb6
TppBen <animaben9@gmail.com>
2026-05-13 06:41:54 +0100

    Update integer warning

--- pythoncalculator.py
+++ pythoncalculator.py
@@ -1,11 +1,11 @@
 # This is a basic calculator made by Ben Hughes in Python
+# Please only put in integers into the program
 # Addition
 print("Would you like to do some addition?")
 answer = input()
 if answer == "yes":
-    # Changed int to float here
-    num1 = float(input("Please enter the first number you would like to add together: "))
-    num2 = float(input("Please enter the second number you would like to add together: "))
+    num1 = int(input("Please enter the first number you would like to add together (It has to be an integer): "))
+    num2 = int(input("Please enter the second number you would like to add together: "))
     print(num1 + num2)
 else:
     print("Okay, understood")
@@ -14,9 +14,8 @@ else:
 print("Would you like to do some multiplication?")
 answer = input()
 if answer == "yes":
-    # Changed int to float here
-    num1 = float(input("Please enter first number you would like to times together: "))
-    num2 = float(input("Please enter second number you would like to times together: "))
+    num1 = int(input("Please enter first number you would like to times together (It has to be an integer): "))
+    num2 = int(input("Please enter second number you would like to times together: "))
     print(num1 * num2)
 else:
     print("Okay, understood")
@@ -25,9 +24,8 @@ else:
 print("Would you like to do some division?")
 answer = input()
 if answer == "yes":
-    # Changed int to float here
-    num1 = float(input("Please enter the first number you would like to divide: "))
-    num2 = float(input("Please enter the second number you would like to divide from the first number: "))
+    num1 = int(input("Please enter the first number you would like to divide (It has to be an integer): "))
+    num2 = int(input("Please enter the second number you would like to divide from the first number: "))
     print(num1 / num2)
 else:
     print("Okay understood")
@@ -36,10 +34,11 @@ else:
 print("Would you like to do some subtraction?")
 answer = input()
 if answer == "yes":
-    # Changed int to float here
-    num1 = float(input("Please enter the first number you would like to subtract: "))
-    num2 = float(input("Please enter the second number you would like to subtract from the first: "))
+    num1 = int(input("Please enter the first number you would like to subtract (It has to be an integer): "))
+    num2 = int(input("Please enter the second number you would like to subtract from the first: "))
     print(num1 - num2)
 else:
     print("Okay, understood")
+
+# Prints regardless of anything when all if and else statements are met
 print("Program finished!")
